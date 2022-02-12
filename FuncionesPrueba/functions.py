60a0da6ceaedeb24c31dee23d3586f674aa2a5ed
Iván L. Murillo <diskman199@gmail.com>
2022-02-12 16:23:18 -0600

    Functiones listas

--- FuncionesPrueba/functions.py
+++ FuncionesPrueba/functions.py
@@ -28,7 +28,44 @@ class TestFunctions:
 
     def get_sphere(self, dimentions):
         result = 0
-        for d in dimentions:
-            result += d**2
+        for x in dimentions:
+            result += x**2
+        return result
+
+    def rosenbrock(self, n, rang):
+        X, Y = self.arange(n, rang)
+        X, Y = np.meshgrid(X, Y)
+        Z = self.get_rosenbrock([X, Y])
+        self.plot_3d(X, Y, Z)
+
+    def get_rosenbrock(self, dimentions):
+        x = dimentions
+        result = 0
+        for i in range(len(dimentions)-1):
+            result += 100 * (x[i+1] - x[i]**2) ** 2 + (x[i] - 1)**2
+        return result
+
+    def rastringin(self, n, rang):
+        X, Y = self.arange(n, rang)
+        X, Y = np.meshgrid(X, Y)
+        Z = self.get_rastringin([X, Y])
+        self.plot_3d(X, Y, Z)
+
+    def get_rastringin(self, dimentions, A = 10):
+        result = A * len(dimentions)
+        print(result)
+        for x in dimentions:
+            result += x**2 - A * np.cos(2 * np.pi * x)
         return result
     
+    def quartic(self, n, rang):
+        X, Y = self.arange(n, rang)
+        X, Y = np.meshgrid(X, Y)
+        Z = self.get_quartic([X, Y])
+        self.plot_3d(X, Y, Z)
+
+    def get_quartic(self, dimentions):
+        result = 0
+        for x in dimentions:
+            result += x**4
+        return result
--- FuncionesPrueba/main.py
+++ FuncionesPrueba/main.py
@@ -4,6 +4,9 @@ def main():
     functions = TestFunctions()
 
     functions.sphere(2, 5.12)
+    functions.rosenbrock(2, 2.048)
+    functions.rastringin(2, 5.12)
+    functions.quartic(2, 1.28)
 
 if __name__ == '__main__':
     main()
