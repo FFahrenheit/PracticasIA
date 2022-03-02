b3814116f51971769b9db96c6db729803511bce6
Iván L. Murillo <diskman199@gmail.com>
2022-03-02 17:01:28 -0600

    Muchisima mejor implementación de algoritmo genetico binario

--- AlgoritmoGeneticoBinario/coins.py
+++ AlgoritmoGeneticoBinario/coins.py
@@ -1,43 +1,19 @@
 class Coins:
-    def __init__(self, denominaciones, suma):
-        self._suma = suma
+    def __init__(self, denominaciones):
         self._denominaciones = denominaciones
 
     def f(self, longitud_gen, cromosoma):
-
-        total = self._suma
-        ultimo = False
-        ultimo_restado = False
-        ultimo_valor = 0 
+        total = 0
         juntos = 1
-        
-        for n, (i, j) in enumerate(zip(cromosoma[::2], cromosoma[1::2])):
-            x = n*2         #Indice i  
-            y = n*2 + 1     #Indice j
-
-            if i and j:
-                total -= (self._denominaciones[x] + self._denominaciones[y])
-                ultimo = True
-                ultimo_restado = True
-                juntos += 1
-                ultimo_valor = self._denominaciones[y]
-
-            elif not i and not j:
-                total -= (self._denominaciones[x] + self._denominaciones[y]) 
-                ultimo = False
-            
-            elif i:
-                if ultimo:
-                    if ultimo_restado:
-                        total -= self._denominaciones[x]
-                    else:
-                        total -= (self._denominaciones[x] + ultimo_valor)
+        anterior = False
+        for index, gen in enumerate(cromosoma):
+            if gen:
+                total += self._denominaciones[index]
+                if anterior:
                     juntos += 1
-                ultimo = False
-            
-            elif j:
-                ultimo = True
-                ultimo_valor = self._denominaciones[y]
-                ultimo_restado = False
+                anterior = True
+            else:
+                anterior = False
 
-        return total / juntos
+        return total ** (1/juntos)
+
--- AlgoritmoGeneticoBinario/main.py
+++ AlgoritmoGeneticoBinario/main.py
@@ -7,12 +7,12 @@ def main():
     5, 5, 20, 1, 1, 5, 10, 10, 10, 1, 5, 2, 1, 2, 10, 20, 2, 10, 10, 20, 5, 10, 1, 2, 1, 5,
     20, 2, 5, 1, 5, 10, 2, 5, 10, 2, 1, 1, 1, 10, 20, 10, 20, 2, 2, 10, 20, 10, 1, 1, 5, 2 ]
 
-    individuos = 64
+    individuos = 50
     alelos = len(monedas)
-    tamano_gen = 2
-    generaciones = 2_000
+    tamano_gen = 1
+    generaciones = 2_500
     factor_mutacion = 0.01
-    fila = coins.Coins(monedas, sum(monedas))
+    fila = coins.Coins(monedas)
     
     ag = AG.AG(individuos, alelos, tamano_gen, generaciones, factor_mutacion, fila)
     final = ag.run()
@@ -20,6 +20,7 @@ def main():
     posiciones = {}
     tomadas = {}
     total_tomado = 0
+    suma = sum(monedas)
     for i, valor in enumerate(monedas):
         if valor in posiciones:
             posiciones[valor].append(i)
@@ -32,15 +33,20 @@ def main():
                 tomadas[valor].append(i)
             else:
                 tomadas[valor] = [ i ]
-
+    print('#'*55)
+    
+    print("Moneda\t\tTomadas\t\tPorcentaje\tTotal")
     for k in sorted(posiciones):
         total = len(posiciones[k])
         tomada = len(tomadas.get(k, []))
         porcentaje = round(tomada/total * 100, 2)
-        print(f"${k}\t {tomada}/{total}\t {porcentaje}%\t ${tomada * k}")
+        print(f"${k}\t\t{tomada}/{total}\t\t{porcentaje}%\t\t${tomada * k}")
     
     total_tomadas = len([None for moneda in final if moneda == 1])
-    print(f"Total: ${total_tomado}. {total_tomadas}/{len(monedas)} = {round(total_tomadas/len(monedas)*100,2)}%")
+    print('-'*55)
+    print(f"Monedas\t\t{total_tomadas}/{len(monedas)}\t\t{round(total_tomadas/len(monedas)*100,2)}%\t\t${total_tomado}")
+    print(f"Valor\t\t{total_tomado}/{suma}\t\t{round(total_tomado/suma*100, 2)}%\t\t${total_tomado}")
+    print(f"Maximo\t\t{len(monedas)}/{len(monedas)}\t\t100.00%\t\t${suma}")
 
 def test():
     monedas = [ 1, 20, 5, 1, 2, 5, 5, 1, 5, 2, 2, 1, 10, 5, 10, 5, 20, 20, 20, 5, 1, 1, 20,
@@ -48,7 +54,7 @@ def test():
     5, 5, 20, 1, 1, 5, 10, 10, 10, 1, 5, 2, 1, 2, 10, 20, 2, 10, 10, 20, 5, 10, 1, 2, 1, 5,
     20, 2, 5, 1, 5, 10, 2, 5, 10, 2, 1, 1, 1, 10, 20, 10, 20, 2, 2, 10, 20, 10, 1, 1, 5, 2 ]
 
-    fila = coins.Coins(monedas, sum(monedas))
+    fila = coins.Coins(monedas)
     p1 = [1 for i in range(len(monedas))]
 
     print(f"{p1} = {fila.f(2, p1)}")
