4a841e7e3c52ecbf2e505eaeae2fb03448ff3c62
Ivan L. Murillo <diskman199@gmail.com>
2023-09-07 10:32:52 -0600

    Implementado algoritmo y graficas

--- CorreccionDeError/mainwindow.py
+++ CorreccionDeError/mainwindow.py
@@ -1,11 +1,15 @@
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QMainWindow, QMessageBox
+from PySide2.QtCore import Slot, QTimer
 from ui_mainwindow import Ui_MainWindow
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+from random import random
+import numpy as np
 
 LEFT_CLICK = 1
 RIGHT_CLICK = 3
+MAX_ITERATIONS = 100
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -15,20 +19,123 @@ class MainWindow(QMainWindow):
 
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
-
         self.ui.plot_area.addWidget(self.canvas)
+        
+        self.ui.begin_button.clicked.connect(self.start_algorithm)
+        self.ui.clear_button.clicked.connect(self.clear_all)
+
         self.draw_chart()
         self.initilize_algorithm()
-    
+
+    @Slot()
+    def clear_all(self):
+        if self.is_running:
+            return
+        
+        plt.clf()
+        self.draw_chart()
+        self.canvas.draw()
+        self.ui.b_label.setText("-")
+        self.ui.w1_label.setText("-")
+        self.ui.w2_label.setText("-")
+        self.initilize_algorithm()
+
+    @Slot()
+    def start_algorithm(self):
+        if self.is_running:
+            return
+        
+        if len(self.data) <= 0:
+            return self.print_error("Datos insuficientes","No hay suficientes patrones de entrenamiento")
+        
+        n = str(self.ui.learning_rate.text())
+
+        try:
+            n = float(n)
+        except ValueError:
+            return self.print_error("Datos erroneos", "Constante de entrenamiento debe ser numerica")
+        
+        if n <= 0 or n >= 1:
+            return self.print_error("Datos erroneos", "Constante de entrenamiento debe estar entre 0 y 1")
+        
+        self.is_running = True
+        self.run_algorithm(n)
+
+    def run_algorithm(self, n):
+        b = random()
+        w1 = random()
+        w2 = random()
+
+        error = True
+        iterations = 0
+        self.plot_solutions = []
+
+        while error and iterations < MAX_ITERATIONS:
+            error = False
+            for pattern in self.data:
+                x1, x2, y = pattern
+
+                v = x1*w1 + x2*w2 + b
+                u = self.activation_function(v)
+                e = y - u
+
+                if e != 0:
+                    error = True
+
+                w1 = w1 + n*e*x1
+                w2 = w2 + n*e*x2
+                b = b + n*e
+            
+            # Plot solution
+            m = -w1/w2
+            c = -b/w2
+
+            x_plot = np.array([-1, 1])
+            y_plot = m*x_plot + c
+            self.plot_solutions.append([x_plot, y_plot, [w1, w2, b]])
+
+            iterations += 1
+        
+        print(f"Algoritmo finalizado con éxito {not error} en {iterations} iteraciones")
+        self.plot_index = 0
+        self.plot_with_delay()
+
+    def plot_with_delay(self):
+        if self.plot_index < len(self.plot_solutions):
+            
+            if self.plot_index != 0:
+                self.ax.lines.pop()
+            
+            x, y, solution = self.plot_solutions[self.plot_index]
+            w1, w2, b = solution
+
+            self.ax.plot(x, y, linestyle='-', color='g')
+            self.canvas.draw()
+
+            self.ui.iteration_label.setText(str(self.plot_index + 1))
+            self.ui.b_label.setText(str(round(b, 3)))
+            self.ui.w1_label.setText(str(round(w1, 3)))
+            self.ui.w2_label.setText(str(round(w2, 3)))
+
+            self.plot_index += 1
+            QTimer.singleShot(500, self.plot_with_delay)
+        else:
+            self.is_running = False
+            self.plot_solutions = []
+
+    def activation_function(self, v):
+        return 0 if v < 0 else 1
+
     def initilize_algorithm(self):
+        self.is_running = False
         self.data = []
 
     def handle_onclick(self, event):
-        if event.inaxes is None:
+        if event.inaxes is None or self.is_running:
             return
         
         x, y = event.xdata, event.ydata
-
+        
         if event.button == LEFT_CLICK:
             result = 1
             self.ax.plot(x, y, 'bo')
@@ -62,4 +169,7 @@ class MainWindow(QMainWindow):
 
         # Display the empty plot
         self.cid = self.figure.canvas.mpl_connect('button_press_event', self.handle_onclick)
-        plt.show()
+        # plt.show()
+
+    def print_error(self, title, message):
+        QMessageBox.critical(self, title, message)
--- CorreccionDeError/ui_mainwindow.py
+++ CorreccionDeError/ui_mainwindow.py
@@ -17,7 +17,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(556, 290)
+        MainWindow.resize(559, 340)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout_2 = QGridLayout(self.centralwidget)
@@ -30,25 +30,27 @@ class Ui_MainWindow(object):
         self.groupBox_4.setObjectName(u"groupBox_4")
         self.gridLayout_3 = QGridLayout(self.groupBox_4)
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.w2_label = QLabel(self.groupBox_4)
-        self.w2_label.setObjectName(u"w2_label")
-        font = QFont()
-        font.setPointSize(14)
-        self.w2_label.setFont(font)
-
-        self.gridLayout_3.addWidget(self.w2_label, 4, 2, 1, 1)
-
         self.b_label = QLabel(self.groupBox_4)
         self.b_label.setObjectName(u"b_label")
+        font = QFont()
+        font.setPointSize(14)
         self.b_label.setFont(font)
 
-        self.gridLayout_3.addWidget(self.b_label, 5, 2, 1, 1)
+        self.gridLayout_3.addWidget(self.b_label, 6, 2, 1, 1)
 
-        self.w1_label = QLabel(self.groupBox_4)
-        self.w1_label.setObjectName(u"w1_label")
-        self.w1_label.setFont(font)
+        self.label = QLabel(self.groupBox_4)
+        self.label.setObjectName(u"label")
+        font1 = QFont()
+        font1.setPointSize(12)
+        self.label.setFont(font1)
 
-        self.gridLayout_3.addWidget(self.w1_label, 3, 2, 1, 1)
+        self.gridLayout_3.addWidget(self.label, 0, 1, 1, 1)
+
+        self.begin_button = QPushButton(self.groupBox_4)
+        self.begin_button.setObjectName(u"begin_button")
+        self.begin_button.setFont(font1)
+
+        self.gridLayout_3.addWidget(self.begin_button, 9, 1, 1, 2)
 
         self.label_3 = QLabel(self.groupBox_4)
         self.label_3.setObjectName(u"label_3")
@@ -56,40 +58,53 @@ class Ui_MainWindow(object):
 
         self.gridLayout_3.addWidget(self.label_3, 4, 1, 1, 1)
 
-        self.label_4 = QLabel(self.groupBox_4)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setFont(font)
+        self.clear_button = QPushButton(self.groupBox_4)
+        self.clear_button.setObjectName(u"clear_button")
+        self.clear_button.setFont(font1)
 
-        self.gridLayout_3.addWidget(self.label_4, 5, 1, 1, 1)
-
-        self.pushButton_2 = QPushButton(self.groupBox_4)
-        self.pushButton_2.setObjectName(u"pushButton_2")
-
-        self.gridLayout_3.addWidget(self.pushButton_2, 6, 1, 1, 2)
-
-        self.begin_button = QPushButton(self.groupBox_4)
-        self.begin_button.setObjectName(u"begin_button")
-
-        self.gridLayout_3.addWidget(self.begin_button, 7, 1, 1, 2)
-
-        self.label = QLabel(self.groupBox_4)
-        self.label.setObjectName(u"label")
-        font1 = QFont()
-        font1.setPointSize(10)
-        self.label.setFont(font1)
-
-        self.gridLayout_3.addWidget(self.label, 0, 1, 1, 1)
-
-        self.learning_rate = QLineEdit(self.groupBox_4)
-        self.learning_rate.setObjectName(u"learning_rate")
-
-        self.gridLayout_3.addWidget(self.learning_rate, 0, 2, 1, 1)
+        self.gridLayout_3.addWidget(self.clear_button, 8, 1, 1, 2)
 
         self.label_2 = QLabel(self.groupBox_4)
         self.label_2.setObjectName(u"label_2")
         self.label_2.setFont(font)
 
         self.gridLayout_3.addWidget(self.label_2, 3, 1, 1, 1)
+
+        self.label_4 = QLabel(self.groupBox_4)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setFont(font)
+
+        self.gridLayout_3.addWidget(self.label_4, 6, 1, 1, 1)
+
+        self.learning_rate = QLineEdit(self.groupBox_4)
+        self.learning_rate.setObjectName(u"learning_rate")
+        self.learning_rate.setFont(font1)
+
+        self.gridLayout_3.addWidget(self.learning_rate, 0, 2, 1, 1)
+
+        self.w2_label = QLabel(self.groupBox_4)
+        self.w2_label.setObjectName(u"w2_label")
+        self.w2_label.setFont(font)
+
+        self.gridLayout_3.addWidget(self.w2_label, 4, 2, 1, 1)
+
+        self.w1_label = QLabel(self.groupBox_4)
+        self.w1_label.setObjectName(u"w1_label")
+        self.w1_label.setFont(font)
+
+        self.gridLayout_3.addWidget(self.w1_label, 3, 2, 1, 1)
+
+        self.label_5 = QLabel(self.groupBox_4)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setFont(font)
+
+        self.gridLayout_3.addWidget(self.label_5, 1, 1, 1, 1)
+
+        self.iteration_label = QLabel(self.groupBox_4)
+        self.iteration_label.setObjectName(u"iteration_label")
+        self.iteration_label.setFont(font)
+
+        self.gridLayout_3.addWidget(self.iteration_label, 1, 2, 1, 1)
 
 
         self.gridLayout.addWidget(self.groupBox_4, 0, 1, 1, 1)
@@ -105,7 +120,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 556, 21))
+        self.menubar.setGeometry(QRect(0, 0, 559, 21))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -120,14 +135,17 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Presione click izquierdo para poner \"1\" y click derecho para poner \"0\"", None))
         self.groupBox_4.setTitle(QCoreApplication.translate("MainWindow", u"Datos de entrenamiento", None))
-        self.w2_label.setText(QCoreApplication.translate("MainWindow", u"-", None))
         self.b_label.setText(QCoreApplication.translate("MainWindow", u"-", None))
-        self.w1_label.setText(QCoreApplication.translate("MainWindow", u"-", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u03c92", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"b", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Limpiar", None))
-        self.begin_button.setText(QCoreApplication.translate("MainWindow", u"Iniciar", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Raz\u00f3n de aprendizaje", None))
+        self.begin_button.setText(QCoreApplication.translate("MainWindow", u"Iniciar", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\u03c92", None))
+        self.clear_button.setText(QCoreApplication.translate("MainWindow", u"Limpiar", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u03c91", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"b", None))
+        self.learning_rate.setText(QCoreApplication.translate("MainWindow", u"0.4", None))
+        self.w2_label.setText(QCoreApplication.translate("MainWindow", u"-", None))
+        self.w1_label.setText(QCoreApplication.translate("MainWindow", u"-", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"\u00c9poca", None))
+        self.iteration_label.setText(QCoreApplication.translate("MainWindow", u"-", None))
     # retranslateUi
 
